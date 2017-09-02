7a411a04c634df823af9cd52e482bd4732e023b5
Logan Murdock <logtheman@gmail.com>
2017-09-02 11:37:29 -0700

    Resuming work. Styling the footer to match styles in form and header.

--- client/src/landing_page_components/InfoCard.jsx
+++ client/src/landing_page_components/InfoCard.jsx
@@ -2,17 +2,14 @@ import React from 'react';
 
 const InfoCard = props => {
   return (
-    <div className="container info-card">
-      <div className="row">
-        <div className="col-2  info-card-icon">
+    <div className="info-card">
+      <div className="col-12">
+        <div className="info-card-headline">
           <i className={props.icon} aria-hidden="true"></i>
+          {' '}{' '}{props.headLine}
+
         </div>
-        <div className="col-10">
-          <div className="info-card-headline">
-            {props.headLine}
-          </div>
-          {props.children}
-        </div>
+        {props.children}
       </div>
     </div>
   );
--- client/src/landing_page_components/InfoCardsRow.jsx
+++ client/src/landing_page_components/InfoCardsRow.jsx
@@ -5,22 +5,22 @@ import InfoCard from './InfoCard';
 
 const InfoCardsRow = props => {
   return ( 
-    <div className="row">
-      <div className="col-md-4">
-        <InfoCard headLine="Be Empowered!" icon="fa fa-clock-o">
-          Schedule work around your own life.
-        </InfoCard>
-      </div>
-      <div className="col-md-4">
-        <InfoCard headLine="Enjoy Work!" icon="fa fa-cart-plus">
-          Spend time shopping, exploring new things and being active.
-        </InfoCard>
-      </div>
-      <div className="col-md-4">
-        <InfoCard headLine="Get Paid!" icon="fa fa-money">
-          Get paid weekly. Work Sundays to maximize your hours and pay.
-        </InfoCard>
-      </div>
+    <div className="row info-card-row text-center">
+        <div className="col-md-4 mx-0 py-3">
+          <InfoCard headLine="Be Empowered!" icon="fa fa-clock-o">
+            Schedule work around your own life.
+          </InfoCard>
+        </div>
+        <div className="col-md-4 mx-0 py-3">
+          <InfoCard headLine="Enjoy Work!" icon="fa fa-cart-plus">
+            Spend time shopping, exploring new things and being active.
+          </InfoCard>
+        </div>
+        <div className="col-md-4 mx-0 py-3">
+          <InfoCard headLine="Get Paid!" icon="fa fa-money">
+            Get paid weekly. Work Sundays to maximize your hours and pay.
+          </InfoCard>
+        </div>
     </div>
   );
 }
