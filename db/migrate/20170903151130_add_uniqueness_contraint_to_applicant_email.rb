1af3b228e3c75272f22c514a242b5b79d0f0a7e5
Logan Murdock <logtheman@gmail.com>
2017-09-03 08:11:57 -0700

    starting work again

---
+++ db/migrate/20170903151130_add_uniqueness_contraint_to_applicant_email.rb
@@ -0,0 +1,4 @@
+class AddUniquenessContraintToApplicantEmail < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170903043013) do
+ActiveRecord::Schema.define(version: 20170903150255) do
 
   create_table "applicants", force: :cascade do |t|
     t.string   "first_name"
