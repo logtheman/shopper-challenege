05f03c23fbd1ec8cefc872afea5f965851c507c8
Logan Murdock <logtheman@gmail.com>
2017-09-03 21:42:15 -0700

    finish for the night. Close to getting the correct output

--- db/test_script.js
+++ db/test_script.js
@@ -3,29 +3,41 @@ const file = "applicants.sqlite3";
 const db = new sqlite3.Database(file); 
 const startDate = process.argv[2];
 const endDate = process.argv[3];
-let applicantRows = [];
+let hash = {};
 
 function getStartofWeek(d) {
   d = new Date(d);
-  const day = d.getDay(),
-      diff = d.getDate() - day + (day == 0 ? -6:1); // adjust when day is sunday
-  return new Date(d.setDate(diff));
+  const day = d.getDay();
+  const diff = d.getDate() - day + (day == 0 ? -6:1); // adjust when day is sunday
+  return new Date(d.setDate(diff)).toISOString().slice(0,10);
 }
-  
 
-db.all(`SELECT * FROM applicants WHERE created_at >= ${startDate} AND ${endDate} LIMIT 10`, function(err, rows) {  
-      console.log("err", err);
-      applicantRows = rows;
-      // console.log("rows", rows);
-      rows.forEach(function (row) {  
-        let formattedDate = new Date(row.created_at);
-        let week = getStartofWeek(row.created_at);
-        week = week.toString().split('T')[0];
+function checkIfWeekInList(week, workflowState){
+  if(hash[week]){
+    if(hash[week][workflowState]){
+      hash[week][workflowState] += 1;
+    }else{
+      hash[week][workflowState] = 1;
+    }
+  }else{
+    hash[week] = {}; 
+    hash[week][workflowState] = 1;
+  }
+  //inc
+}
 
-          console.log(row.first_name, row.last_name, row.created_at, week);  
-      })  
-  });   
+db.all(`SELECT * FROM applicants WHERE created_at >= ${startDate} AND ${endDate} LIMIT 100`, function(err, rows) {  
+    // console.log("rows", rows);
+    rows.forEach(function (row) {  
+      let formattedDate = new Date(row.created_at);
+      let week = getStartofWeek(row.created_at);
+      checkIfWeekInList(week, row.workflow_state);
 
+      // console.log(row.first_name, row.last_name, row.workflow_state, week);  
+    })  
+console.log('\n\n RESULTS' ,hash);
+    
+});   
 db.close();  
 
 
